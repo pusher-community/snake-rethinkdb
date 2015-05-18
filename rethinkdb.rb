322219a9593d871b1b863d45a2871d4686217074
Jamie Patel <jamie@notespublication.com>
2015-05-18 11:45:27 +0100

    Pass in conn to sinatra

--- rethinkdb.rb
+++ rethinkdb.rb
@@ -7,11 +7,11 @@ Pusher.url = ENV["PUSHER_RETHINK_URL"]
 
 include RethinkDB::Shortcuts
 
-r.connect(
+$conn = r.connect(
   host: "localhost",
   port: 28015,
   db: 'snake',
-).repl
+)
 
 PLAYERS = r.table("players")
 
@@ -20,12 +20,12 @@ LEADERBOARD = PLAYERS.order_by({index: r.desc('high_score')}).limit(5)
 
 EventMachine.next_tick do
 
-  LIVE_SCORES.changes.em_run do |err, change|
+  LIVE_SCORES.changes.em_run($conn) do |err, change|
     updated_player = change["new_val"]
     Pusher["scores"].trigger("new_score", updated_player)
   end
 
-  LEADERBOARD.changes.em_run do |err, change|
+  LEADERBOARD.changes.em_run($conn) do |err, change|
     updated_player = change["new_val"]
     Pusher["scores"].trigger("new_high_score", updated_player)
   end
